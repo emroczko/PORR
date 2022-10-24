aea9dce387b35c957768d197250093b38317ccb5
Eryk <emroczko@me.com>
2022-10-24 19:52:16 +0200

    Added social net class

---
+++ Sequential/socialNet.cpp
@@ -0,0 +1,13 @@
+class SocialNet {
+    private:
+        int verticesCount;
+
+    public:
+        SocialNet(int verticesCount);
+
+};
+
+SocialNet::SocialNet(int verticesCount)
+{
+    this->verticesCount = verticesCount;
+}
