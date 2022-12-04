135a03dbfabd7ed8754f6ca0babaab6bf883d113
Eryk <emroczko@me.com>
2022-12-04 23:55:07 +0100

    Added printing iterators, to see openmp in action

--- Sequential/Graph.cpp
+++ Sequential/Graph.cpp
@@ -1,4 +1,5 @@
 #include "Graph.h"
+#include <omp.h>
 #include <iostream>
 #include <vector>
 #include <map>
@@ -7,8 +8,10 @@ void Graph::countFriends() {
     for (int u = 0; u < vertices_count; u++) {
         int counter = 0;
         for (int v = 0; v < vertices_count; v++)
-            if (adjacency_matrix[u][v])
+            if (adjacency_matrix[u][v]) {
                 ++counter;   
+                // std::cout<< "friend: " << u << " " << v << std::endl;
+            }
         // std::cout << "Standard: Person: " << u + 1 << " has " << counter << " friends" << std::endl;
     }
 }
@@ -18,8 +21,10 @@ void Graph::countFriendsEfficiently(int threads_number) {
     for (int u = 0; u < vertices_count; u++) {
         int counter = 0;
         for (int v = 0; v < vertices_count; v++)
-            if (adjacency_matrix[u][v])
+            if (adjacency_matrix[u][v]) {
                 ++counter; 
+                // std::cout<< "friend: " << u << " " << v << std::endl;
+            }
         // std::cout << "Standard: Person: " << u << " has " << counter << " friends" << std::endl;
     }
 }
