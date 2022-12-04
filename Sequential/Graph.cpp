a6a4032fbba1f5bd2b321b33f5102d82346067a3
Eryk <emroczko@me.com>
2022-12-04 17:31:34 +0100

    Fix in openmp

--- Sequential/Graph.cpp
+++ Sequential/Graph.cpp
@@ -18,8 +18,8 @@ void Graph::countFriendsEfficiently(int threads_number) {
     for (int u = 0; u < vertices_count; u++) {
         int counter = 0;
         for (int v = 0; v < vertices_count; v++)
-            ++counter;
-            
+            if (adjacency_matrix[u][v])
+                ++counter; 
         // std::cout << "Standard: Person: " << u << " has " << counter << " friends" << std::endl;
     }
 }
