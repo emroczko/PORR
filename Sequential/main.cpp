ebb75dc814325b1c39d5d9ae385ff6ce2740dedc
Marcin Piotrowski <mpio@netcompany.com>
2022-12-11 19:02:49 +0100

    commented out filesystem

--- Sequential/main.cpp
+++ Sequential/main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <memory>
 #include <chrono>
-#include <filesystem>
+// #include <filesystem>
 #include "Graph.h"
 
 #define MAX_THREADS 10
@@ -18,7 +18,6 @@ std::unique_ptr<Graph> createGraphFromFile(const std::string &fileName)
 
   while (std::getline(fileToRead, line))
   {
-
     if (line[0] == '%')
       continue;
 
@@ -35,7 +34,7 @@ std::unique_ptr<Graph> createGraphFromFile(const std::string &fileName)
     {
       int id, key;
       ss >> id >> key;
-      socialNetwork->addEdge(std::pair(id, key));
+      socialNetwork->addEdge(std::pair<int, int>(id, key));
     }
     ss.clear();
   }
@@ -48,18 +47,18 @@ std::unique_ptr<Graph> createGraphFromFile(const std::string &fileName)
 int main(int argc, char** argv)
 {
 
-  if (argc != 2 ) {
-    std::cout<<"You have to pass the dataset file name as a 1st argument"<<std::endl;
-    exit(1);
-  }
+  // if (argc != 2 ) {
+  //   std::cout<<"You have to pass the dataset file name as a 1st argument"<<std::endl;
+  //   exit(1);
+  // }
 
-  std::filesystem::path dataset_path { argv[1] };
-  if (!std::filesystem::exists(dataset_path)) {
-    std::cout<<"Given dataset does not exists!"<<std::endl;
-    exit(1);
-  }
+  // std::filesystem::path dataset_path { argv[1] };
+  // if (!std::filesystem::exists(dataset_path)) {
+  //   std::cout<<"Given dataset does not exists!"<<std::endl;
+  //   exit(1);
+  // }
 
-  std::unique_ptr<Graph> graph = createGraphFromFile("../Datasets/gplus.txt");
+  std::unique_ptr<Graph> graph = createGraphFromFile("C:\\school\\przedmioty\\PORR\\PORR\\Datasets\\a.txt");
   std::cout << "Graph created" << std::endl;
 
   for (int threads = 1; threads <= MAX_THREADS; threads++)
@@ -67,13 +66,13 @@ int main(int argc, char** argv)
     auto start_seq = std::chrono::high_resolution_clock::now();
     graph->countFriends();
     auto stop_seq = std::chrono::high_resolution_clock::now();
-    auto duration_seq = std::chrono::duration_cast<std::chrono::microseconds>(stop_seq - start_seq);
+    auto duration_seq = std::chrono::duration_cast<std::chrono::milliseconds>(stop_seq - start_seq);
     
     auto start_par = std::chrono::high_resolution_clock::now();
     graph->countFriendsEfficiently(threads);
     auto stop_par = std::chrono::high_resolution_clock::now();
-    auto duration_par = std::chrono::duration_cast<std::chrono::microseconds>(stop_par - start_par);
+    auto duration_par = std::chrono::duration_cast<std::chrono::milliseconds>(stop_par - start_par);
 
-    std::cout << "Sequential : parallel processing: " << duration_seq.count() << " : "<< duration_par.count() << " microseconds" << std::endl;
+    std::cout << "Sequential : parallel processing: " << duration_seq.count() << " : "<< duration_par.count() << " milliseconds" << std::endl;
   }
 }
