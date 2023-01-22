73d72cd58bd4d15a604ab96d74a8ca0b1306990c
Eryk <emroczko@me.com>
2023-01-22 01:44:01 +0100

    Added needed arguments for context load tests

--- Spark/src/test/java/porr/spark/SparkApplicationTests.java
+++ Spark/src/test/java/porr/spark/SparkApplicationTests.java
@@ -3,11 +3,10 @@ package porr.spark;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(args = {"--add-opens" , "java.base/sun.nio.ch=ALL-UNNAMED"})
 class SparkApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
